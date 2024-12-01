07826bb359aaa74a9cb2e42a732bb2e72190d597
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-01 16:49:03 -0500

    api functionality added from rapidapi

--- client/src/pages/CoffeeSearch.tsx
+++ client/src/pages/CoffeeSearch.tsx
@@ -2,20 +2,29 @@ import { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 
 interface Coffee {
-  title: string;
+  _id: string;
+  name: string;
+  image: string;
   description: string;
-  ingredients: string[];
-  // Add other properties as needed
 }
 
-function CoffeeList() {
+function CoffeeSearch() {
   const [coffees, setCoffees] = useState<Coffee[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [query, setQuery] = useState<string>('');
 
-  const fetchCoffees = async (endpoint: string) => {
+  const fetchCoffees = async (query: string) => {
     try {
-      const response = await fetch(endpoint);
+      const response = await fetch(`https://starbucks-coffee-db2.p.rapidapi.com/api/recipes?name=${query}`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'x-rapidapi-ua': 'RapidAPI-Playground',
+          'x-rapidapi-key': '110e9351a0msh7b3e8441780311ep1213ccjsn8688a123eb0c',
+          'x-rapidapi-host': 'starbucks-coffee-db2.p.rapidapi.com'
+        }
+      });
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -28,7 +37,7 @@ function CoffeeList() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    fetchCoffees(`https://api.sampleapis.com/coffee/${query}`);
+    fetchCoffees(query);
   };
 
   return (
@@ -36,7 +45,7 @@ function CoffeeList() {
       <h1>Search Coffees</h1>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
-          <Form.Label>Enter coffee type (e.g., hot, iced)</Form.Label>
+          <Form.Label>Enter coffee type (e.g., mocha)</Form.Label>
           <Form.Control
             type="text"
             value={query}
@@ -50,11 +59,11 @@ function CoffeeList() {
       </Form>
       {error && <div>Error: {error}</div>}
       <ul>
-        {coffees.map((coffee, index) => (
-          <li key={index}>
-            <h2>{coffee.title}</h2>
+        {coffees.map((coffee) => (
+          <li key={coffee._id}>
+            <h2>{coffee.name}</h2>
             <p>{coffee.description}</p>
-            <p>Ingredients: {coffee.ingredients.join(', ')}</p>
+            <img className="coffee-thumbnail" src={coffee.image} alt={coffee.name} />
           </li>
         ))}
       </ul>
@@ -62,4 +71,4 @@ function CoffeeList() {
   );
 }
 
-export default CoffeeList;
+export default CoffeeSearch;
