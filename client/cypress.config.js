a640808cf97dfe925c76e3b885b8a84f9decff73
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-03 19:44:08 -0500

    github actions testing

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,19 +1,38 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require("cypress");
 
-const is_prod = process.env.PORT;
-
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3333",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: is_prod ? 'http://localhost:10000' : 'http://localhost:5173',
   },
 
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
+
+
+// import { defineConfig } from 'cypress';
+
+// const is_prod = process.env.PORT;
+
+// export default defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//     baseUrl: is_prod ? 'http://localhost:10000' : 'http://localhost:5173',
+//   },
+
+//   component: {
+//     devServer: {
+//       framework: 'react',
+//       bundler: 'vite',
+//     },
+//   },
+// });
