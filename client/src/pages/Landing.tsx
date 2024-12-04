306eb3dbe0eb07d7e222af74919e701aef270387
delucianina <delucianina@gmail.com>
2024-12-04 15:23:13 -0500

    added landing page animation

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 // import { useQuery } from '@apollo/client';
 
@@ -6,6 +7,20 @@ import { Row, Col, Container } from 'react-bootstrap';
 
 function Landing() {
   // const { data: coffeeData } = useQuery(GET_ALL_COFFEES);
+
+  const aboutDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (aboutDiv.current) {
+        const aboutDivTop = aboutDiv.current.getBoundingClientRect().top;
+        if (aboutDivTop < window.innerHeight) {
+          aboutDiv.current.style.opacity = '1';
+          aboutDiv.current.style.transform = 'translateX(0)';
+        }
+      }
+    });
+  }, []);
 
   return (
     <Container fluid={true}>
@@ -22,9 +37,9 @@ function Landing() {
         <div className='landing-section'>
           <div className="side-flex">
             <div className="search-recipes">
-            <button className="landing-btn">
-              <a href="/coffee-info" className="white-text"><h4>SEARCH RECIPES</h4></a>
-            </button>
+              <button className="landing-btn">
+                <a href="/coffee-info" className="white-text"><h4>SEARCH RECIPES</h4></a>
+              </button>
             </div>
             {/* <h3>GET BREWING</h3>
             <a href="/" className="white-text landing-link">SEARCH RECIPES</a> */}
@@ -37,7 +52,7 @@ function Landing() {
 
       <Row className="center-content">
         <Col className="landing-section">
-          <div className="about-img">
+          <div className="about-img" ref={aboutDiv}>
             <button className="landing-btn">
               <a href="/about" className="white-text"><h4>ABOUT US</h4></a>
             </button>
