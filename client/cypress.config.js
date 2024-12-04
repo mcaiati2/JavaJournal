63366f1cde802f1b9c87b2c70e858d11ce4d49d2
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-03 20:12:57 -0500

    testing configured for appropriate ports

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: is_prod ? 'http://localhost:10000' : 'http://localhost:3333',
+    baseUrl: is_prod ? 'http://localhost:10000' : 'http://localhost:5173',
   },
 
   component: {
